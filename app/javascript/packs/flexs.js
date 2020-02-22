4bf55018fc88bb7c122daaa9048f9379c09872bb
Ваше Имя <you@example.com>
2020-02-22 23:29:33 +0200

    Product show fixes

--- app/javascript/packs/flexs.js
+++ app/javascript/packs/flexs.js
@@ -1,5 +1,6 @@
 // Can also be used with $(document).ready()
-$(window).load(function() {
+// $(window).load(function() {
+$(document).ready(function() {
     $('.flexslider').flexslider({
         animation: "slide",
         controlNav: "thumbnails"
